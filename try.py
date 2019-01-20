bbb607f38e4113a742d01a8cdf543e916b3f3b0c
Yuanchen Liu <yuanchen0218@gmail.com>
2019-01-20 00:27:46 -0800

    start

---
+++ try.py
@@ -0,0 +1,26 @@
+class treeNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = next
+    
+    def preDFS(root):
+        if not root:
+            return
+        print(root.val)
+        preDFS(root.left)
+        preDFS(root.right)
+
+    def preDFS(root):
+        if not root:
+            return
+        stack = []
+        ptr = root
+        while stack or root:
+            if ptr:
+                print(ptr.val)
+                stack.append(ptr)
+                ptr = ptr.left
+            else:
+                ptr = stack.pop()
+                ptr = ptr.right
+                
